0bbb27b3d2b8f1abf83d313acacf78808f9dc11a
Nina Kazzim <kazzim@icloud.com>
2019-06-05 03:01:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(arr)
+  size = arr.size
+  count = 0
+  arr2 = []
 
-
+  while count < size
+    ele = arr[count]
+    arr2 << yield(ele)
+    count += 1
+  end
+  
+  return arr2
+  
+end
